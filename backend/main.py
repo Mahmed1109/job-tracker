74a5897b794a95538b658b2ef70e07db99f26599
Mahmed1109 <m.ahmed110900@gmail.com>
2026-02-26 13:34:04 +0000

    Day 4: POST endpoint to add job applications

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 from backend.database import engine, SessionLocal
-from backend import models
+from backend import models, schemas
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -17,3 +17,21 @@ def get_db():
 @app.get("/")
 def root():
     return {"message": "Job Tracker API is running"}
+
+@app.post("/applications/", response_model=schemas.JobApplicationResponse)
+def create_application(application: schemas.JobApplicationCreate, db: Session = Depends(get_db)):
+    db_application = models.JobApplication(
+        company=application.company,
+        job_title=application.job_title,
+        status=application.status,
+        job_url=application.job_url,
+        location=application.location,
+        salary=application.salary,
+        notes=application.notes,
+        date_applied=application.date_applied,
+        follow_up_date=application.follow_up_date
+    )
+    db.add(db_application)
+    db.commit()
+    db.refresh(db_application)
+    return db_application
